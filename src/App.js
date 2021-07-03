912294fa2afeae21a1593eb57e5281b36986fd46
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-07-04 00:18:55 +0530

    create a chat room

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { nanoid } from "nanoid";
+
+const socket = io.connect("http://localhost:5000");
+const username = nanoid(4);
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChart = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { message, username });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Socket IO Chat</h1>
+        {chat.map((payload, id) => {
+          return (
+            <p key={id}>
+              {payload.message}: <span>id: {payload.username}</span>
+            </p>
+          );
+        })}
+        <form onSubmit={sendChart}>
+          <input
+            type='text'
+            name='chat'
+            placeholde='send text'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></input>
+          <button type='submit'>Send</button>
+        </form>
       </header>
     </div>
   );
